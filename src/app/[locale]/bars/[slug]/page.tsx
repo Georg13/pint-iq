dd5d22c561170d44605d59c3733298ee1f06ce7b
Georgii Kav <geokav87@gmail.com>
2026-06-22 18:35:01 +0300

    fix: make bars page a valid module

---
+++ src/app/[locale]/bars/[slug]/page.tsx
@@ -0,0 +1,6 @@
+export default function BarSlugPage() {
+  return <div className="text-2xl font-bold text-center mt50">Страница бара (в разработке) <br /> 
+  
+  Но быть может когданибудь <br /> Она увидет белый свет
+  </div>;
+}
